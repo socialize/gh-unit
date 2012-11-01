3c1f1107ef4b892757e8df8ac945cf878d330a46
Nate Griswold <nategriswold@gmail.com>
2012-11-01 15:24:13 -0700

    Add headers export script
    For use as subproject / lib instead of framework

---
+++ Scripts/export-public-headers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+[ -n "${CONFIGURATION_BUILD_DIR}" ] || { echo "Run from Xcode"; exit 1; }
+
+DEST="${CONFIGURATION_BUILD_DIR}/GHUnitIOS"
+
+mkdir -p "${DEST}"
+
+find . -type d -name Examples -prune \
+  -o -type d -name Tests -prune \
+  -o -type d -name build -prune \
+  -o -name '*.h' -print0 | xargs -0 -I{} cp {} "${CONFIGURATION_BUILD_DIR}/GHUnitIOS"
