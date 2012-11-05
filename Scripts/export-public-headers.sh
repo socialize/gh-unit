c535406ac75540f9eefce277118b2593febf8bab
Nathaniel Griswold <nategriswold@gmail.com>
2012-11-05 12:16:36 -0800

    Fix header export

--- Scripts/export-public-headers.sh
+++ Scripts/export-public-headers.sh
@@ -6,7 +6,7 @@ DEST="${CONFIGURATION_BUILD_DIR}/GHUnitIOS"
 
 mkdir -p "${DEST}"
 
-find . -type d -name Examples -prune \
+find "${PROJECT_DIR}/.." -type d -name Examples -prune \
   -o -type d -name Tests -prune \
   -o -type d -name build -prune \
-  -o -name '*.h' -print0 | xargs -0 -I{} cp {} "${CONFIGURATION_BUILD_DIR}/GHUnitIOS"
+  -o -name '*.h' -print0 | xargs -0 -I{} cp -v {} "${DEST}"
